05b8d5445ab35edcd3b8eaa64a6a3d68d39b422e
freddy <freddydre8@gmail.com>
2024-12-17 21:54:34 -0500

    output

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "kubeconfig_command" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.eks_cluster.name}"
+}
