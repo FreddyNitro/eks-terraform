5a0ba50df62b55b935e9a9115fd93aae58af9ea5
Freddy <freddydre8@gmail.com>
2024-12-23 20:13:34 -0500

    main 2nd update

--- main.tf
+++ main.tf
@@ -6,14 +6,104 @@ terraform {
 }
 
 module "vpc" {
-  path = "./vpc.tf"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "eks-vpc"
+
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.available.names
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
 }
 
-module "iam" {
-  path = "./iam.tf"
+data "aws_availability_zones" "available" {}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-module "eks" {
-  path = "./eks.tf"
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  ])
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = each.value
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.cluster_name}-node-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ])
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = each.value
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = module.vpc.private_subnets
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_role_policies]
+}
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = [var.node_instance_type]
+
+  depends_on = [aws_iam_role_policy_attachment.eks_node_role_policies]
 }
 
