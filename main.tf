b180d44de39882ff29a61cfd264d338accd0db1d
Freddy <freddydre8@gmail.com>
2024-12-23 19:59:42 -0500

    update main

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ terraform {
 }
 
 module "vpc" {
-  source = "./vpc.tf"
+  path = "./vpc.tf"
 }
 
 module "iam" {
-  source = "./iam.tf"
+  path = "./iam.tf"
 }
 
 module "eks" {
-  source = "./eks.tf"
+  path = "./eks.tf"
 }
 
