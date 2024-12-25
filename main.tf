bb155bf4ffd4e9910bf5d70f66f09f5c54ca2049
Freddy <freddydre8@gmail.com>
2024-12-24 19:55:51 -0500

    udated main#

--- main.tf
+++ main.tf
@@ -12,25 +12,6 @@ resource "aws_vpc" "eks_vpc" {
   }
 }
 
-resource "aws_subnet" "public_subnets" {
-  for_each = toset(var.public_subnets)
-  vpc_id   = aws_vpc.eks_vpc.id
-  cidr_block = each.value
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Public Subnet ${each.value}"
-  }
-}
-
-resource "aws_subnet" "private_subnets" {
-  for_each = toset(var.private_subnets)
-  vpc_id   = aws_vpc.eks_vpc.id
-  cidr_block = each.value
-  tags = {
-    Name = "Private Subnet ${each.value}"
-  }
-}
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.eks_vpc.id
   tags = {
