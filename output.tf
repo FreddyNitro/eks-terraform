41bb994d7e561dd09c4488c19d87e3c484e2ddf9
Freddy <freddydre8@gmail.com>
2025-01-02 21:45:25 -0500

    updated @

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  value = aws_eks_cluster.eks.name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "node_role_arn" {
